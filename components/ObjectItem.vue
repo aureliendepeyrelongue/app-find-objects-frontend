2db8574b1c29e6c832e6ce26c364c8be04616ef2
37009904 <37009904@parisnanterre.fr>
2021-03-31 10:44:28 +0200

    feat: add objectItem component and render feature to objects-list page.

---
+++ components/ObjectItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="card">
+      <div class="card-image">
+        <img src="http://localhost:5000/images/object2.jpg" />
+      </div>
+      <div class="card-content">
+        <span class="card-title"
+          >Objet : {{ object.category.value }} {{ object.state }}</span
+        >
+        <p>Description : {{ object.category.description }}</p>
+        <p>Lieux : {{ locationComputed }}</p>
+        <p></p>
+      </div>
+      <div class="card-action">
+        <a href="#">Voir en détails</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    object: Object
+  },
+  data() {
+    return {};
+  },
+
+  computed: {
+    locationComputed() {
+      return `${this.object.location.value}, ${this.object.location.locationAddress.street}, ${this.object.location.locationAddress.city}, ${this.object.location.locationAddress.zipcode}`;
+    }
+  }
+};
+</script>
+
+<style scoped></style>
